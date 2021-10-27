c33b15e333f4f8574f2461cf973f217d19280932
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-10-28 00:21:16 +0300

    upgrade Task.tsx

--- src/state/Task.tsx
+++ src/state/Task.tsx
@@ -1,31 +1,31 @@
-import {changeTaskNameAC, changeTaskStatusAC, removeTaskAC} from "./tasks-reducer";
+import {changeTaskNameAC, changeTaskStatusAC, changeTaskStatusTC, deleteTaskTC, removeTaskAC} from "./tasks-reducer";
 import {ChangeInput} from "../ChangeInput";
-import {TaskType} from "../Todolist";
-import { useDispatch } from "react-redux";
-import React, {ChangeEvent} from 'react';
-import { Checkbox, IconButton } from "@material-ui/core";
-import { Delete } from "@material-ui/icons";
-import { useCallback } from "react";
 
-type TaskPropsType={
+import {useDispatch} from "react-redux";
+import React, {ChangeEvent, useCallback} from 'react';
+import {Checkbox, IconButton} from "@material-ui/core";
+import {Delete} from "@material-ui/icons";
+import {TaskStatuses, TaskType} from "../API/todolists-api";
+
+type TaskPropsType = {
     todolistId: string
     task: TaskType
 }
 
-export const Task = React.memo( (props:TaskPropsType)=> {
+export const Task = React.memo((props: TaskPropsType) => {
     const dispatch = useDispatch();
-    const onClickHandler =useCallback( () => dispatch(removeTaskAC(props.task.id, props.todolistId)),[])
-    const onChangeHandler = useCallback( (e: ChangeEvent<HTMLInputElement>) => {
+    const onClickHandler = useCallback(() => dispatch(deleteTaskTC(props.todolistId, props.task.id)), [])
+    const onChangeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
         let newIsDoneValue = e.currentTarget.checked;
-        dispatch(changeTaskStatusAC(props.task.id, newIsDoneValue, props.todolistId))
-    },[])
+        dispatch(changeTaskStatusTC(props.task.id, newIsDoneValue ? TaskStatuses.Completed : TaskStatuses.New, props.todolistId))
+    }, [])
 
-    const changename= useCallback((newTitle: string)=> {
+    const changename = useCallback((newTitle: string) => {
         dispatch(changeTaskNameAC(props.task.id, newTitle, props.todolistId))
-    },[changeTaskNameAC])
+    }, [changeTaskNameAC])
 
-    return <div key={props.task.id} className={props.task.isDone ? "is-done" : ""}>
-        <Checkbox onChange={onChangeHandler} checked={props.task.isDone}/>
+    return <div key={props.task.id} className={props.task.status===TaskStatuses.Completed ? 'is-done' : ""}>
+        <Checkbox onChange={onChangeHandler} checked={props.task.status === TaskStatuses.Completed}/>
         <ChangeInput title={props.task.title} onChange={changename}/>
 
         <IconButton onClick={onClickHandler}>
