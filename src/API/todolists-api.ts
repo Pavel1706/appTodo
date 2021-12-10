6625d6bd27075cd57027af19620ce3858baae648
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-12-10 13:50:51 +0300

    change todolists-api.ts

--- src/API/todolists-api.ts
+++ src/API/todolists-api.ts
@@ -10,6 +10,19 @@ const instance = axios.create({
 
 })
 
+export const authAPI = {
+    login(data: LoginParamsType) {
+        return instance.post<LoginParamsType, AxiosResponse<ResponseType<{ userId?: number }>>>('auth/login', data)
+    },
+    me() {
+        return instance.get< ResponseType<{ id: number, email: string, login: string }>>('auth/me')
+    },
+    logOut(){
+        return instance.delete< ResponseType>('auth/login')
+    },
+}
+
+
 export const todolistsAPI = {
     getTodolists() {
         return instance.get<TodolistType[]>('todo-lists')
@@ -23,20 +36,27 @@ export const todolistsAPI = {
     updateTodolist(id: string, title: string) {
         return instance.put<{ title: string }, AxiosResponse<ResponseType>>(`todo-lists/${id}`, {title})
     },
-    getTasks(todolistId:string){
+    getTasks(todolistId: string) {
         return instance.get<GetTasksResponse>(`todo-lists/${todolistId}/tasks`)
     },
-    deleteTask(todolistId:string,taskId:string){
+    deleteTask(todolistId: string, taskId: string) {
         return instance.delete<ResponseType>(`todo-lists/${todolistId}/tasks/${taskId}`)
     },
-    createTask(title: string,todolistId:string){
-      return instance.post<{title:string}, AxiosResponse<ResponseType<{item:TaskType}>>>(`todo-lists/${todolistId}/tasks`,{title})
+    createTask(title: string, todolistId: string) {
+        return instance.post<{ title: string }, AxiosResponse<ResponseType<{ item: TaskType }>>>(`todo-lists/${todolistId}/tasks`, {title})
     },
-    updateTask(todolistId:string, taskId: string,model: UpdateTaskModelType){
+    updateTask(todolistId: string, taskId: string, model: UpdateTaskModelType) {
         return instance.put<UpdateTaskModelType, AxiosResponse<ResponseType<{ item: TaskType }>>>(`todo-lists/${todolistId}/tasks/${taskId}`, model)
     }
 }
 
+
+export type LoginParamsType = {
+    email: string
+    password: string
+    rememberMe: boolean
+    captcha?: string
+}
 
 export type ResponseType<D = {}> = {
     resultCode: number
@@ -75,12 +95,14 @@ export type UpdateTaskModelType = {
     startDate: string
     deadline: string
 }
+
 export enum TaskStatuses {
     New = 0,
     InProgress = 1,
     Completed = 2,
     Draft = 3
 }
+
 export enum TaskPriorities {
     Low = 0,
     Middle = 1,
