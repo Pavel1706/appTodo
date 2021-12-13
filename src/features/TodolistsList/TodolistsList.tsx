5da6a2c133bb54011205cacfefc419e30e0d3735
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-12-13 13:24:32 +0300

    change TodolistsList.tsx

--- src/features/TodolistsList/TodolistsList.tsx
+++ src/features/TodolistsList/TodolistsList.tsx
@@ -12,47 +12,53 @@ import {Grid, Paper} from "@material-ui/core";
 import {AddItem} from "../../components/AddItem/AddItem";
 import {Todolist} from "./Todolist/Todolist";
 import {FilterValuesType} from "../../app/App";
+import {Navigate} from "react-router-dom";
 
-type TodolistsListType ={
-
+type TodolistsListType = {
+    demo?: boolean
 }
 
 
+export const TodolistsList: React.FC<TodolistsListType> = ({demo = false}) => {
+    const isLoggedIn = useSelector<AppRootState, boolean>(state => state.auth.isLoggedIn)
 
-
-export const TodolistsList: React.FC<TodolistsListType>=(props)=> {
-
-    useEffect(()=> {
+    useEffect(() => {
+        if (demo) {
+            return
+        }
         dispatch(getTodolistsTC())
 
-    },[])
+    }, [])
 
     console.log('app is called')
     const dispatch = useDispatch();
     const todolists = useSelector<AppRootState, Array<TodolistDomainType>>(state => state.todolists)
 
 
-
-    const changeFilter=useCallback((todolistId: string, value: FilterValuesType)=> {
+    const changeFilter = useCallback((todolistId: string, value: FilterValuesType) => {
         const action = changeTodolistFilterAC(todolistId, value)
         dispatch(action)
-    },[dispatch])
+    }, [dispatch])
 
-    const removeTodolist=useCallback((id: string)=> {
+    const removeTodolist = useCallback((id: string) => {
         dispatch(deleteTodolistTC(id))
         // const action = removeTodolistAC(id)
         // dispatch(action)
 
-    },[dispatch])
+    }, [dispatch])
 
     const addItem = useCallback((Newtitle: string) => {
         dispatch(createTodolistsTC(Newtitle))
     }, [dispatch])
 
-    const  changeNameTodo=useCallback(( id: string, newTitle: string)=> {
+    const changeNameTodo = useCallback((id: string, newTitle: string) => {
         const action = changeTodolistTitleTC(id, newTitle)
         dispatch(action)
-    },[])
+    }, [])
+
+    if(!isLoggedIn){
+        return <Navigate to={'/login'}/>
+    }
 
 
     return (<>
@@ -62,17 +68,15 @@ export const TodolistsList: React.FC<TodolistsListType>=(props)=> {
         <Grid container spacing={3}>
             {
                 todolists.map(tl => {
-                    return(
+                    return (
                         <Grid item>
                             <Paper style={{padding: '20px'}}>
-                                <Todolist
-                                    changeNameTodo={changeNameTodo}
-                                    key={tl.id}
-                                    id={tl.id}
-                                    title={tl.title}
-                                    changeFilter={changeFilter}
-                                    filter={tl.filter}
-                                    removeTodolist={removeTodolist}
+                                <Todolist demo={demo}
+                                          todolist={tl}
+                                          changeNameTodo={changeNameTodo}
+                                          key={tl.id}
+                                          changeFilter={changeFilter}
+                                          removeTodolist={removeTodolist}
                                 />
                             </Paper>
                         </Grid>)
