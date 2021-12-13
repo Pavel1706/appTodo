ec912c6dcff8d898859494691a956e67153573f8
Pavel1706 <80953762+Pavel1706@users.noreply.github.com>
2021-12-13 13:22:21 +0300

    change store.ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,12 +3,14 @@ import {todolistsReducer} from "../features/TodolistsList/todolists-reducer";
 import {tasksReducer} from "../features/TodolistsList/tasks-reducer";
 import thunk from 'redux-thunk';
 import {appReducer} from "./app-reducer";
+import {authReducer} from "../features/Login/login-reducer";
 
 
 const rootReducer = combineReducers({
     todolists: todolistsReducer,
     tasks: tasksReducer,
     app: appReducer,
+    auth: authReducer,
 })
 
 
